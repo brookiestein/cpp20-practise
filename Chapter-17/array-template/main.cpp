7ac8b8c6ede7201588498f59fb3825394b44f25e
brookiestein <brookie1@protonmail.com>
2021-05-15 07:11:06 -0400

    Practicing a little bit more.
    Adding a member to add an item without resizing the array.
    Making a helper function not to repeat myself when adding an item
    both at the beginning as at the end of the Array.
    And the constructor which initializes an Array from an initializer list
    makes confusion when making an Array with a fixed size and with an
    initializer list, so for now it's commented, but must be fixed.

--- Chapter-17/array-template/main.cpp
+++ Chapter-17/array-template/main.cpp
@@ -16,19 +16,26 @@ public:
                 m_size {size}
         {}
 
-        explicit Array(std::initializer_list<T> init_list)
-                : m_size {init_list.size()},
-                m_elements { new T[init_list.size()] }
-        {
-                size_t i {};
-                for (auto& item : init_list)
-                        m_elements[i++] = item;
-        }
+        /* FIXME: Having this constructor makes confusion when initializing an Array
+         * with an initializer_list and with a fixed size. */
+        /* explicit Array(std::initializer_list<T> init_list) */
+        /*         : m_size {init_list.size()}, */
+        /*         m_elements { new T[init_list.size()] } */
+        /* { */
+        /*         size_t i {}; */
+        /*         for (auto& item : init_list) */
+        /*                 m_elements[i++] = item; */
+        /* } */
 
         /* I've not been able to implement the copy constructor yet... */
-        Array(const Array& src) = delete;
+        Array(const Array& src)
+                : Array {src.m_size}
+        {
+                for (size_t i {}; i < m_size; ++i)
+                        m_elements[i] = src.m_elements[i];
+        }
 
-        ~Array<T>() { delete[] m_elements; }
+        ~Array() { delete[] m_elements; }
 
         void forEach(size_t perline = 5) const noexcept
         {
@@ -63,47 +70,82 @@ public:
         T& operator[](size_t index) { return at(index); }
         const T& operator[](size_t index) const { return at(index); }
 
+        /* Add an item at the end of the array taking in account that array has enough space for adding it. */
+        void add(T item)
+        {
+                if (position == m_size)
+                        throw std::out_of_range {
+                                "There's no enough space for adding the new item: "
+                                + std::to_string(item)
+                        };
+                m_elements[position++] = item;
+        }
+
+        /* Resets the static counter and so start to add items from the beginning of
+         * the array. */
+        void reset_counter() noexcept { position = 0; }
+
         void push_back(T item)
         {
-                T* new_array { new T[(m_size * sizeof(T)) + sizeof(item)] };
-                size_t i;
-                for (i = 0; i < m_size; ++i)
-                        new_array[i] = at(i);
+                auto new_array { get_reallocated_ptr() };
+                size_t i {};
+                copy_and_assign(new_array, i, m_size);
                 new_array[i] = item;
-                delete[] m_elements;
-                m_elements = new_array;
-                ++m_size;
         }
 
         void push_front(T item)
         {
-                T* new_array { new T[(m_size * sizeof(T)) + sizeof(item)] };
+                auto new_array { get_reallocated_ptr() };
                 size_t i {};
-                new_array[i] = item;
-                for (i = 1; i <= m_size; ++i)
-                        new_array[i] = at(i - 1);
-                delete[] m_elements;
-                m_elements = new_array;
-                ++m_size;
+                new_array[i++] = item;
+                copy_and_assign(new_array, i, m_size + 1);
+                /* Adding an item at the beginning must move the position because
+                 * e.g, imagine you have this array: 1, 2, 3. And you add 0 at the
+                 * beginning, so it is now: 0, 1, 2, 3. If position were 3, adding
+                 * another item with the add() member will replace the quarter item,
+                 * but if it's incremented here, it will have 4 as value and no item
+                 * will be replaced. */
+                ++position;
         }
 private:
         T* m_elements;
         size_t m_size;
+        size_t position {};
+
+        T* get_reallocated_ptr() const { return new T[(m_size * sizeof(T)) + sizeof(T)]; }
+
+        void copy_and_assign(T*& arr, size_t& where_to_start, const size_t& where_to_finish)
+        {
+                for (size_t i {}; where_to_start < where_to_finish; ++where_to_start, ++i)
+                        arr[where_to_start] = at(i);
+                delete [] m_elements;
+                m_elements = arr;
+                ++m_size;
+        }
 };
 
 int
 main()
 {
-        Array<int> arr {1, 2, 3};
+        /* Array<int> arr {1, 2, 3}; */
+        Array<int> arr {5};
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
 
         std::cout << "Original array:\n";
         arr.forEach();
 
-        arr.push_back(4);
+        arr.push_back(6);
         std::cout << "After adding an item at the end:\n";
         arr.forEach();
 
         arr.push_front(0);
         std::cout << "After adding an item at the beginning:\n";
+        arr.forEach();
+
+        arr.add(4);
+        arr.add(5);
+        std::cout << "After adding two items where the position was:\n";
         arr.forEach();
 }
