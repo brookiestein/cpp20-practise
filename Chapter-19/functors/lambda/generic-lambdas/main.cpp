680f977aa52a279bc0886ddb4d14e185f404dc4c
brookiestein <brookie1@protonmail.com>
2021-06-11 22:19:54 -0400

    Practising the newer way of making a lambda in C++ 20.

--- Chapter-19/functors/lambda/generic-lambdas/main.cpp
+++ Chapter-19/functors/lambda/generic-lambdas/main.cpp
@@ -1,5 +1,16 @@
 /* A generic lambda is a lambda expression that have a placeholder
  * such like: auto, auto&, const auto& in its parameter list. */
+/* In C++ 20, and further you can specify the type of a generic lambda,
+ * such like you can do with a template. */
+/* Why would you want to do that? Well, that technique disallow the
+ * compiler to make implicit conversions such like:
+ * auto greater { [] (const auto& a, const auto& b) { return a > b; } };
+ * greater(5, 10.0);
+ * Basically, you're passing an integer and a double to the lambda.
+ * In that case, the compiler makes an implicit conversion in order to
+ * make that operation possible.
+ * Writing the lambda in the newer C++ 20's way, makes that impossible, unless
+ * you cast one of the two values in order to have both equal. */
 
 #include <iostream>
 #include <string>
@@ -7,7 +18,8 @@
 int
 main()
 {
-        auto greater { [] (const auto& a, const auto& b) { return a > b; } };
+        /* auto greater { [] (const auto& a, const auto& b) { return a > b; } }; */
+        auto greater { [] <typename T> (const T& a, const T& b) { return a > b; } };
         std::cout << "Is 10 greater than 5? " << std::boolalpha
                 << greater(10, 5) << std::endl;
 
